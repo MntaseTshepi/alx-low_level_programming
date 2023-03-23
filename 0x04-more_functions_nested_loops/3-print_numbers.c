45fe5e8d659a3fd4efc9bf3b297dbc7a415b8c96
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-03-23 06:11:37 -0700

    Task 3- Prints numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,6 @@
 /**
  * print_numbers - Function prints numbers from 0 to 9
  *
- * Return: Always 0 (Success)
  */
 
 
@@ -16,5 +15,4 @@ void print_numbers(void)
 		_putchar(i + '0');
 	}
 	_putchar('\n');
-	return (0);
 }
