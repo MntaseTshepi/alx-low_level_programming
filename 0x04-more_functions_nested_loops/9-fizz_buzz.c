bbec8e4839255ec5a2cf4b05a60e52ab1d742ee1
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-03-23 11:53:53 -0700

    Task 9 - Prints Fizz, Buzz and FizzBuzz for multiples of 3, 5 and (3 and 5)

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 1 to 100, followed by a new line.
+ * For the multiples of three Fizz is printed.
+ * For the multiples of five Buzz is printed.
+ * For the multiples of three and five FizzBuzz is printed.
+ */
+
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -9,3 +9,4 @@ Task 5 - Writing a function that prints numbers from 0 to 14 ten times.
 Task 6 - Writing a function that draws a straight line in the terminal.
 Task 7 - Writing a function that draws a diagonal line in the terminal.
 Task 8 - Writing a fucntion that prints a square follwed by a new line.
+Task 9 - Writing a program that prints Fizz for multiples of 3, Buzz for multiples of 5 and FizzBuzz for multiples of 3 and 5.
