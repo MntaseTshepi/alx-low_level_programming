c268ff3663648582bc5920414db880f415d343a4
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-03-23 05:41:00 -0700

    Task 2- Multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies the two inputted values a and b
+ * @a: The first digit
+ * @b: The second digit
+ *
+ * Return: The product of a and b
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
