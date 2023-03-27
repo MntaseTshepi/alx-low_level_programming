20dbff43c9992247cc4a39025b9ebebcf64bebc2
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-03-27 16:20:49 -0700

    Task 7- Prints half of a string, folowed by a new line

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - Function prints half of a string, followed by a new line
+ * @str: String input
+ *
+ * Retur: void
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = len / 2 ; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
