3a1ca5849b3c211b1dfd939f1a386410360937db
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-03-27 11:45:20 -0700

    Updated Task 4- Prints out a string in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,8 @@ void print_rev(char *s)
 {
 	int len;
 	int i;
-	int j;
+
+	len = 0
 
 	while (*s != '\0')
 	{
@@ -18,9 +19,7 @@ void print_rev(char *s)
 		s++;
 	}
 
-	j = len;
-
-	for (i = j - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
