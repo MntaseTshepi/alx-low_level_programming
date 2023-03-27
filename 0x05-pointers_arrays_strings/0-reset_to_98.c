8cada99eac8478fde347beb77e2145c5ba538ffc
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-03-27 07:27:26 -0700

    Task 0 - Updates the value the pointer variable points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value the pointer points to, to 98
+ * @n: variable of the pointer
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0x05.C - Pointers, arrays and strings
+
+Task 0 - Writing a function that takes a pointer to an in as a parameter and updates its value to a given number.
