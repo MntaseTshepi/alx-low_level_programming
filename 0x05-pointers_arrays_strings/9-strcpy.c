311e16515c823bcae055d85cefc4cb0fb72b689b
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-03-28 09:18:31 -0700

    Updated Task 9 - Copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int len = 0;
 
-	for (i = 0; src[i] < '\0'; i++)
+	while (*(src + len) != '\0')
 	{
-		dest[i] = src[i];
+		*(dest + len) = *(src + len);
+		len++;
 	}
 
+	*(dest + len) = '\0';
 	return (dest);
 }
