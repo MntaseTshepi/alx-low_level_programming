022ba21e0bda8b94a73fe7e64b23e1f33d65245d
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-03-28 08:46:41 -0700

    Updated Task 7- prints half a string with a new line

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,19 +10,29 @@
 void puts_half(char *str)
 {
 	int len = 0;
-	int i;
+	int i, n;
 
 	while (str[len] != '\0')
 	{
 		len++;
 	}
 
+	n = (len - 1) / 2;
 
-	for (i = len / 2; i < len; i++)
+	if (len % 2 == 0)
 	{
-		_putchar(str[i]);
+		for (i = (len / 2); i < len; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
-
+	else
+	{
+		for (i = n + 1; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
 	_putchar('\n');
 }
 
