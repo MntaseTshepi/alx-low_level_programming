1b00420429409824ec3399ca56f72e28e224ee5c
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-03-29 13:34:19 -0700

    Task 2 - Copies a string, using n characters from src and copies to dest

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - Function copies a string
+ * @dest: Destination string input.
+ * @src: Source string input.
+ * @n: Number of characters to be copied.
+ *
+ * Return: Pointer to resulting dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	if (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
