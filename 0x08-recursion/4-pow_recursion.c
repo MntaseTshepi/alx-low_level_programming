0d776a129c06c157c69b0f4d2e4abf99178313c6
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-05 11:00:08 -0700

    Task 4 - Returns the value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y.
+ * @x: The base number.
+ * @y: The exponent.
+ *
+ * Return: -1 if @y is less than 0, 1 if @y is equal to 0
+ * @x if @y is equal to 1 or the value of @x to the power of @y.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -4,3 +4,4 @@ Task 0 - Writing a function that prints a string, followed by a new line.
 Task 1 - Writing a function that prints a string in reverse.
 Task 2 - Writing a function that returns the length of a string.
 Task 3 - Writing a fucntion that returns the factorial of a given number.
+Task 4 - Writing a function that returns the value x raised by the power y.
