04ac9ac5ec9ee1c2ad9a679014eeb1cdaf7993f5
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-06 03:59:42 -0700

    Task 6 - Checks if a number input is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Checks if the input integer is a prime number
+ * @n: The input integer.
+ *
+ * Return: 0 if @n is not a prime number or 1 if it is.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (prime_num_func(n, 2));
+}
+
+/**
+ * prime_num_func - Checks if num is divisible
+ * @num: The number input
+ * @div: The divisor.
+ *
+ * Return: 0 if @num is divisible or 1 if it is not.
+ */
+
+int prime_num_func(int num, int div)
+{
+	if ((n % div) == 0)
+	{
+		return (0);
+	}
+	else if (div > _sqrt_recursion(num))
+	{
+		return (1);
+	}
+
+	return (prime_num_func(num, div + 1));
+}
