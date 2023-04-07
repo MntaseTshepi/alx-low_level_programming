72aa223b2a5762855663f4d1f2d95f1953b83bac
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-07 07:18:23 -0700

    Task 2 - Prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,16 +3,18 @@
 /**
  * main - Entry point.
  * @argc: The count of arguments supplied to program.
- * @argv: Array of pointers to the string.
+ * @argv: Array of pointers.
  *
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
+	int i;
+
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[0]);
+		printf("%s\n", argv[i]);
 	}
 
 	return (0);
