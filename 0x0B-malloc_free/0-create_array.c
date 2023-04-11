6006c8d00821911f697211f70fd3456df85d4a41
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-11 07:53:42 -0700

    Task 0 - Creates an array of characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Function creates an array of chars.
+ * @size: The size of the array.
+ * @c: Fills in the spaces allocated in the array.
+ *
+ * Return: Pointer to array or null if it fails.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *space;
+
+	if (size == 0)
+	{
+		return ('\0');
+	}
+	space = (char *)malloc(size * sizeof(char));
+
+	if (space == '\0')
+	{
+		return ('\0');
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		space[i] = c;
+	}
+
+	return (space);
+}
