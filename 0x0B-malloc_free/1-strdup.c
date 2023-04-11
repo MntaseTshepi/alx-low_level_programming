0acb27b61d8706b1db762771121222aba6ee4936
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-11 10:11:44 -0700

    Task 1 - Duplicates a string input

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer which contains a copy of a given string.
+ * @str: The string input.
+ *
+ * Return: Pointer to duplicated string or null if memory insufficient.
+ */
+
+char *_strdup(char *str)
+{
+	int count = 0;
+	int i;
+	char *s;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+
+	s = (char *)malloc(count * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < count; i++)
+	{
+		s[i] = str[i];
+	}
+
+	return (s);
+}
+
+
