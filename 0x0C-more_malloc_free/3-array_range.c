6fe755e3c52b2d40ebd386c56081d8e7d0f49929
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-13 12:09:34 -0700

    Task 3 - Creates an array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - Creates an array of integers.
+ * @min: The minimum input value.
+ * @max: The maximum input value.
+ * Return: Pointer to the newly created array.
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *val;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	val = malloc((max - min + min) * sizeof(int) + 1);
+
+	if (val == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; min <= max; i++, min++)
+	{
+		val[i] = min;
+	}
+
+	return (val);
+}
