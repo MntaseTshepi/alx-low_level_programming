ab8c2af160d765ad3d35a20d1ca72c009cf4b956
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-13 06:14:36 -0700

    Task 0 - Returns a pointer with allocated memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Function allocates using malloc.
+ * @b: The number of bytes of allocated memory.
+ *
+ * Return: Pointer to allocated memory or null if fail.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+       	int *val;
+
+
+	val = malloc(b);
+
+	if (val == NULL)
+	{
+		exit(98);
+	}
+
+	return (val);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+0x0C. C - More malloc, free
+
+Task 0 - Writing a fucntion that allocates memory using malloc.
