22ba335918fa35ec1bdc5e82982f996ca302bc14
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-17 10:02:55 -0700

    Task 0- Defining a new type called struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - User defined data type containg information about dogs
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The name of the dog owner.
+ *
+ * Description: The data type collects information relating to dogs
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+
+#endif
