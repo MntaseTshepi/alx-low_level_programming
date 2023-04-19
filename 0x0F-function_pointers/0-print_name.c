7b30d31b0a1708ae896f797b6e72156832b0cd8e
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-19 07:53:12 -0700

    Updated Task 0 - Null inclusion

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,9 +6,13 @@
  * @name: The name input.
  * @f: The pointer to a fucntion.
  *
+ * Return: Void
+ *
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
