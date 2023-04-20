ca35abbcd4419022837c371839e197ea815cd14a
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-20 14:27:09 -0700

    Task 3 - Prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,61 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Function prints anything.
+ * @format: List of the types of agruments passed.
+ *
+ * Return: Void
+ */
+
+void print_all(const char * const format, ...)
+{
+	char c;
+	int i;
+	float f;
+	char *s;
+	int idx = 0;
+
+	va_list args;
+	va_start(args, format);
+
+	while (format[idx] != '\0')
+	{
+		switch (format[idx])
+		{
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(args, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (!s)
+				{
+					printf("(nil)");
+				}
+				else
+				{
+					printf("%s", s);
+				}
+				break;
+			default:
+				idx++;
+				continue;
+		}
+		idx++;
+		if (format[idx] != '\0')
+		{
+			printf(", ");
+		}
+	}
+
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -3,3 +3,5 @@
 Task 0 - Writing a function that returns the sum of all its parameters.
 Task 1 - Writing a function that prints numbers, followed by a new line.
 Task 2 - Writing a function that  prints strings, followed by a new line.
+Task 3 - Writing a  function that prints anything.
+
