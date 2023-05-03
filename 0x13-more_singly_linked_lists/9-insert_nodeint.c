2a6ecbfbf505ac74a2afdd8ca35f8bec98174033
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-05-03 04:07:34 -0700

    Updated Task 9 : Failed checks - code error fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,6 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
 	listint_t *temp = *head;
+	unsigned int i = 0;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL || head == NULL)
@@ -21,15 +22,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
-	idx--;
-
-	while (idx != 0)
+	if (idx == 0)
 	{
-		temp = temp->next;
-		idx--;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	new_node->next = temp->next;
-	temp->next = new_node;
 
-	return (new_node);
+	while (temp && i < idx)
+	{
+		if (i == idx - 1)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
+		else
+		{
+			temp = temp->next;
+		}
+		i++;
+	}
+	return (NULL);
 }
